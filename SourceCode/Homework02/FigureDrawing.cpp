f61b54e3793ebcd10a8a72ee91adc3d9d897f2d6
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2024-06-04 23:38:27 +0300

    adds Shape classes + optimization for coloring shape pixels

--- SourceCode/Homework02/FigureDrawing.cpp
+++ SourceCode/Homework02/FigureDrawing.cpp
@@ -1,113 +1,92 @@
-#include <fstream>
-#include <vector>
-#include <functional>
+#include "PPMImageSaver.h"
 
-static const unsigned IMAGE_WIDTH = 1920;
-static const unsigned IMAGE_HEIGHT = 1080;
-static const unsigned MAX_COLOR_COMPONENT = 255;
+#define min(a,b) ((a)<(b)?(a):(b))
+#define max(a,b) ((a)>(b)?(a):(b))
 
-static const char LINE_SEP = '\n';
-static const char PIXEL_SEP = '\t';
 
-struct Color {
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
+class Shape {
+public: 
+    virtual void createShape(Image& image) const = 0;
 };
 
-typedef std::vector<std::vector<Color>> Image;
-
-void savePixel(std::ofstream& ppm, const Color& pixel);
-void saveImage(const char* imgName, const Image& image);
-void saveImage(std::ofstream& ppm, const Image& image);
-
-
-
-void savePixel(std::ofstream& ppm, const Color& pixel) {
-    // we save control over what to print before and after each pixel
-    ppm << (int)pixel.r << " " << (int)pixel.g << " " << (int)pixel.b; 
-}
-
-void saveImage(const char* imgName, const Image& image) {
-    std::ofstream ppmFileStream(imgName, std::ios::out | std::ios::trunc);
-    ppmFileStream << "P3" << LINE_SEP;
-    ppmFileStream << IMAGE_WIDTH << " " << IMAGE_HEIGHT << LINE_SEP;
-    ppmFileStream << MAX_COLOR_COMPONENT << LINE_SEP;
-
-    saveImage(ppmFileStream, image);
-
-    ppmFileStream.close();
-}
-
-void saveImage(std::ofstream& ppm, const Image& image) {
-    for (int rowId = 0; rowId < IMAGE_HEIGHT; rowId++) {
-        for (int colId = 0; colId < IMAGE_WIDTH; colId++) {
-            savePixel(ppm, image[rowId][colId]);
-            if (colId != IMAGE_WIDTH) {
-                ppm << PIXEL_SEP;
-            }
-        }
-        if (rowId < IMAGE_HEIGHT) { // so we don't save extra lines after the end of the image
-            ppm << LINE_SEP;
-        }
+class Circle : public Shape{
+    int center_x; 
+    int center_y;
+    unsigned radius;
+    Color color;
+    bool isInCircle(int x, int y) const {
+        int diffX = x - center_x;
+        int diffY = y - center_y;
+        return diffX * diffX + diffY * diffY <= radius * radius; 
     }
-}
-
-void createFigure(Image& image, const Color& figureColor, std::function<bool(int, int)> figurePredicate) {
-    // An abstract way of saving the information for figures, since a pixel can either be inside or outside of a figure
-    // This colors in the pixels that are inside the figure only
-    // Additionally it can be made parallel
-    for (int rowId = 0; rowId < IMAGE_HEIGHT; rowId++) {
-        for (int colId = 0; colId < IMAGE_WIDTH; colId++) {
-            if (figurePredicate(colId, rowId)) {
-                image[rowId][colId] = figureColor;
+public:
+    Circle(unsigned radius, const Color& color) : Circle(IMAGE_WIDTH / 2, IMAGE_HEIGHT / 2, radius, color) {}
+    Circle(int center_x, int center_y, unsigned radius, const Color& color) : center_x(center_x), center_y(center_y), radius(radius), color(color) {}
+    
+    void createShape(Image& image) const override {
+        // This limits the number of pixels we consider by only checking the square around the circle
+        for (int rowId = max(center_y - radius, 0); rowId <= min(center_y + radius, IMAGE_HEIGHT - 1); rowId++) { // so we look only in the bounds of the image
+            for (int colId = max(center_x - radius, 0); colId <= min(center_x + radius, IMAGE_WIDTH - 1); colId++) {
+                if (isInCircle(colId, rowId)) {
+                    image[rowId][colId] = color;
+                }
             }
         }
     }
-}
 
+};
 
-void createCircle(Image& image, const Color& circleColor, unsigned radius, int centerX, int centerY) {
-    auto pixelInCircle = [radius, centerX, centerY](int x, int y) {
-        int relativeX = x - centerX;
-        int relativeY = y - centerY;
-        return (relativeX * relativeX + relativeY * relativeY) <= (radius * radius);
-    };
+class Rectangle : public Shape{
+    int center_x;
+    int center_y;
+    unsigned width;
+    unsigned height;
+    Color color;
+public:
+    Rectangle(unsigned width, unsigned height, const Color& color) :
+        Rectangle(IMAGE_WIDTH / 2, IMAGE_HEIGHT / 2, width, height, color) {}
 
-    createFigure(image, circleColor, pixelInCircle);
-}
+    Rectangle(int center_x, int center_y, unsigned width, unsigned height, const Color& color) : 
+        center_x(center_x), center_y(center_y), width(width), height(height), color(color) {
+    }
 
-void createCircle(Image& image, const Color& circleColor, unsigned radius) {
-    int centerX = IMAGE_WIDTH / 2;
-    int centerY = IMAGE_HEIGHT / 2; // default circle center is the center of the image
+    virtual void createShape(Image& image) const override {
+        for (int rowId = max(center_y - height / 2, 0); rowId <= min(center_y + height / 2, IMAGE_HEIGHT - 1); rowId++) { // so we look only in the bounds of the image
+            for (int colId = max(center_x - width / 2, 0); colId <= min(center_x + width / 2, IMAGE_WIDTH - 1); colId++) {
+                image[rowId][colId] = color;
+            }
+        }
+    }
+};
 
-    createCircle(image, circleColor, radius, centerX, centerY);
-}
-
-void createRectangle(Image& image, const Color& rectColor, unsigned width, unsigned height, int centerX, int centerY) {
-    auto pixelInRectangle = [width, height, centerX, centerY](int x, int y) {
-        int relativeX = x - centerX;
-        int relativeY = y - centerY;
-        return (std::abs(relativeX) <= width && std::abs(relativeY) <= height);
-    };
-
-    createFigure(image, rectColor, pixelInRectangle);
-}
+class Square : public Rectangle {
+public:
+    Square(unsigned side, const Color& color) : Rectangle(side, side, color) {}
+    Square(int center_x, int center_y, unsigned side, const Color& color) : Rectangle(center_x, center_y, side, side, color) {}
+    void createShape(Image& image) const override {
+        Rectangle::createShape(image);
+    }
+};
 
 int main()
 {
     Color background{ 50, 50, 50 };
-    Color circleColor{ 10, 200, 10 };
+
+    Color circleColor{ 10, 200, 200 };
     unsigned radiusInPixels = 200;
+    Circle circle(radiusInPixels, circleColor);
 
     Color squareColor{ 200, 20, 20 };
     unsigned side = 300;
+    Square square(IMAGE_WIDTH / 2 - 300, IMAGE_HEIGHT / 2, side, squareColor);
 
     Image image;
     image.assign(IMAGE_HEIGHT, std::vector<Color>(IMAGE_WIDTH, background)); // assigns all the memory in one call to avoid resizes
     
-    createRectangle(image, squareColor, side, side, IMAGE_WIDTH / 2 - 300, IMAGE_HEIGHT / 2);
-    createCircle(image, circleColor, radiusInPixels);
-    saveImage("crt_figures.ppm", image);
+    square.createShape(image);
+    circle.createShape(image);
+
+
+    PPMImageSaver::saveImage("crt_figures.ppm", image);
 }
 
