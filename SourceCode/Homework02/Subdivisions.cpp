dc833519b3e3c90e9408e2f2c5f848065c3c7cd2
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2024-06-04 23:39:57 +0300

    refactors code to use common image saver

--- SourceCode/Homework02/Subdivisions.cpp
+++ SourceCode/Homework02/Subdivisions.cpp
@@ -1,58 +1,5 @@
-#include <fstream>
-#include <vector>
-#include <functional>
 #include <random>
-
-static const unsigned IMAGE_WIDTH = 800;
-static const unsigned IMAGE_HEIGHT = 800;
-static const unsigned MAX_COLOR_COMPONENT = 255;
-
-static const char LINE_SEP = '\n';
-static const char PIXEL_SEP = '\t';
-
-struct Color {
-    int r;
-    int g;
-    int b;
-};
-
-typedef std::vector<std::vector<Color>> Image;
-
-void savePixel(std::ofstream& ppm, const Color& pixel);
-void saveImage(const char* imgName, const Image& image);
-void saveImage(std::ofstream& ppm, const Image& image);
-
-
-
-void savePixel(std::ofstream& ppm, const Color& pixel) {
-    // we save control over what to print before and after each pixel
-    ppm << (int)pixel.r << " " << (int)pixel.g << " " << (int)pixel.b;
-}
-
-void saveImage(const char* imgName, const Image& image) {
-    std::ofstream ppmFileStream(imgName, std::ios::out | std::ios::trunc);
-    ppmFileStream << "P3" << LINE_SEP;
-    ppmFileStream << IMAGE_WIDTH << " " << IMAGE_HEIGHT << LINE_SEP;
-    ppmFileStream << MAX_COLOR_COMPONENT << LINE_SEP;
-
-    saveImage(ppmFileStream, image);
-
-    ppmFileStream.close();
-}
-
-void saveImage(std::ofstream& ppm, const Image& image) {
-    for (int rowId = 0; rowId < IMAGE_HEIGHT; rowId++) {
-        for (int colId = 0; colId < IMAGE_WIDTH; colId++) {
-            savePixel(ppm, image[rowId][colId]);
-            if (colId != IMAGE_WIDTH) {
-                ppm << PIXEL_SEP;
-            }
-        }
-        if (rowId < IMAGE_HEIGHT) { // so we don't save extra lines after the end of the image
-            ppm << LINE_SEP;
-        }
-    }
-}
+#include "PPMImageSaver.h"
 
 Color generateRandomColor(std::mt19937& rng) {
     std::uniform_int_distribution<int> colorPicker(0, MAX_COLOR_COMPONENT);
@@ -110,5 +57,5 @@ int main()
     createSubdivisionsWithNoise(image, horizontalCount, verticalCount);
 
 
-    saveImage("crt_subdivisions_square.ppm", image);
+    PPMImageSaver::saveImage("crt_subdivisions_square.ppm", image);
 }
